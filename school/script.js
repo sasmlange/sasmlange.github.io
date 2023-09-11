7fa0fe6d320152b63b75b22accf5f491d0b7d427
Maximillian Lange <68520673+sasmlange@users.noreply.github.com>
2023-09-11 12:13:30 -0700

    Add files via upload

---
+++ school/script.js
@@ -0,0 +1,67 @@
+var work_shown = false;
+
+function get_value(id) {
+    value = document.getElementById(id).value;
+    if (value === "") {
+        value = 0;
+    }
+
+    return value;
+}
+
+function update_result() {
+    update();
+}
+
+function show_hide_work(hide = false) {
+    if (work_shown || hide) {
+        document.getElementById("work").style.display = 'none';
+        document.getElementById("workbutton").innerHTML = "Show Work";
+        work_shown = false;
+    } else {
+        document.getElementById("work").style.display = 'block';
+        document.getElementById("workbutton").innerHTML = "Hide Work";
+        work_shown = true;
+    }
+}
+
+async function reset_mathjax(id){
+    let mathDiv = document.getElementById(id);
+    await MathJax.startup.promise // make sure initial typesetting has taken place
+    MathJax.typesetClear([mathDiv]) // clear MathJax awareness of this element
+    await MathJax.typesetPromise([mathDiv]) // typeset anew
+}
+
+async function update() {
+    let x1 = get_value("x1");
+    let y1 = get_value("y1");
+    let x2 = get_value("x2");
+    let y2 = get_value("y2");
+    let rise = y1 - y2;
+    let run = x1 - x2;
+    let distance = Math.sqrt(rise ** 2 + run ** 2);
+     
+    show_hide_work(true);
+
+    document.getElementById("result").innerHTML = distance;
+    let data = [{
+        x: [x1, x2],
+        y: [y1, y2],
+        type: 'scatter'
+    }];
+
+    work_shown = false;
+    document.getElementById("work").style.display = 'none';
+      
+    Plotly.newPlot('plot', data);
+
+    document.getElementById("run").innerHTML = `$$ ${x1} - ${x2} = ${run} $$`
+    document.getElementById("rise").innerHTML = `$$ ${y1} - ${y2} = ${rise} $$`
+    document.getElementById("pt").innerHTML = `$$ \\sqrt{${run}^2 + ${rise}^2} = ${distance} $$`
+
+    await reset_mathjax("run");
+    await reset_mathjax("rise");
+    await reset_mathjax("pt");
+}
+
+update_result();
